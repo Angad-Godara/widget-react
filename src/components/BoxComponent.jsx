c3cc5ba2d012ab2b8f30b235fc8f6d8509916884
Angad <80452785+Angad-Godara@users.noreply.github.com>
2023-11-21 19:05:14 +0530

    initial commit

--- src/components/BoxComponent.jsx
+++ src/components/BoxComponent.jsx
@@ -5,9 +5,9 @@ function BoxComponent({apiKey, productIdentifier}) {
     console.log(apiKey);
   return (
     <div className='gs__widget__wrapper'>
-        <h2 class="gs__widget__heading">CARBON FOOTPRINT TRACKER</h2>
-        <img class="gs__widget__test__img" src="https://portalgreenstitch.azurewebsites.net/assets/images/loginLogo.svg" />
-        <p class="gs__widget__content">Mehr Transparenz für fundiertere Entscheidungen: wir wollen Produkte mit dem geringstmöglichen CO2-Fußabdruck anbieten. Zusammen mit Carbonfact haben wir den Footprint unserer Jeans ermittelt.</p>
+        <h2 className="gs__widget__heading">CARBON FOOTPRINT TRACKER</h2>
+        <img className="gs__widget__test__img" src="https://portalgreenstitch.azurewebsites.net/assets/images/loginLogo.svg" />
+        <p className="gs__widget__content">Mehr Transparenz für fundiertere Entscheidungen: wir wollen Produkte mit dem geringstmöglichen CO2-Fußabdruck anbieten. Zusammen mit Carbonfact haben wir den Footprint unserer Jeans ermittelt.</p>
     </div>
   )
 }
