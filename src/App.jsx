7d6e193b89d2d8e2cadc2cfccee9463184d8ec59
Angad <80452785+Angad-Godara@users.noreply.github.com>
2023-11-21 19:03:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BoxComponent from './components/BoxComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [showContent, setShowContent] = useState(false);
+
+  function renderComponents() {
+    const elements = document.querySelectorAll('div[data-cf]');
+    elements.forEach((element) => {
+      const productIdentifier = element.getAttribute('data-productIdentifier');
+      const apiKey = element.getAttribute('data-apiKey');
+
+      // Perform validation of API keys (this might involve making a server request)
+
+      if (validateApiKeys(apiKey, productIdentifier)) {
+        // If validation succeeds, render your React component inside this div
+      setShowContent({apiKey, productIdentifier})
+      }
+    });
+  }
+
+  // Function to validate API keys (sample synchronous validation)
+  function validateApiKeys(apiKey, productIdentifier) {
+    // You might implement server-side validation here
+    // For example, making an API request to validate the keys
+
+    // Sample synchronous validation - Replace this with your actual validation logic
+    if (apiKey && productIdentifier) {
+      // Mock validation by checking if both API key and product identifier are present
+      return true;
+    }
+    return false;
+  }
+  
+  useEffect(() => {
+    renderComponents();
+  },[])
+
+  return (
+    <>
+      {
+        showContent?.apiKey
+        ?
+        <BoxComponent apiKey={showContent?.apiKey} productIdentifier={showContent?.productIdentifier}/>
+        :
+        <div data-apikey="93ce02167b8a4cd3c6189decb476b06e2cd211765ba3521e" data-productidentifier="30005211" data-cf />
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BoxComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './BoxComponent.css'
+
+function BoxComponent({apiKey, productIdentifier}) {
+    console.log(apiKey);
+  return (
+    <div className='gs__widget__wrapper'>
+        <h2 class="gs__widget__heading">CARBON FOOTPRINT TRACKER</h2>
+        <img class="gs__widget__test__img" src="https://portalgreenstitch.azurewebsites.net/assets/images/loginLogo.svg" />
+        <p class="gs__widget__content">Mehr Transparenz für fundiertere Entscheidungen: wir wollen Produkte mit dem geringstmöglichen CO2-Fußabdruck anbieten. Zusammen mit Carbonfact haben wir den Footprint unserer Jeans ermittelt.</p>
+    </div>
+  )
+}
+
+export default BoxComponent
