8bb88f9d3f1641df99f2747247d8f9b83f7def04
Angad <80452785+Angad-Godara@users.noreply.github.com>
2023-11-22 09:29:20 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import BoxComponent from './components/BoxComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [showContent, setShowContent] = useState(false);
-
-  function renderComponents() {
-    const elements = document.querySelectorAll('div[data-cf]');
-    elements.forEach((element) => {
-      const productIdentifier = element.getAttribute('data-productIdentifier');
-      const apiKey = element.getAttribute('data-apiKey');
-
-      // Perform validation of API keys (this might involve making a server request)
-
-      if (validateApiKeys(apiKey, productIdentifier)) {
-        // If validation succeeds, render your React component inside this div
-      setShowContent({apiKey, productIdentifier})
-      }
-    });
-  }
-
-  // Function to validate API keys (sample synchronous validation)
-  function validateApiKeys(apiKey, productIdentifier) {
-    // You might implement server-side validation here
-    // For example, making an API request to validate the keys
-
-    // Sample synchronous validation - Replace this with your actual validation logic
-    if (apiKey && productIdentifier) {
-      // Mock validation by checking if both API key and product identifier are present
-      return true;
-    }
-    return false;
-  }
-  
   useEffect(() => {
+    function renderComponents() {
+      const elements = document.querySelectorAll('div[data-gs][data-productidentifier][data-apikey]');
+      elements.forEach((element) => {
+        const productIdentifier = element.getAttribute('data-productidentifier');
+        const apiKey = element.getAttribute('data-apikey');
+
+        if (validateApiKeys(apiKey, productIdentifier)) {
+          element.classList.add('gs__widget__wrapper');
+          const boxElement = <BoxComponent apiKey={apiKey} productIdentifier={productIdentifier} />;
+          ReactDOM.render(boxElement, element); // Render directly into the selected div element
+        }
+      });
+    }
+
+    function validateApiKeys(apiKey, productIdentifier) {
+      if (apiKey && productIdentifier) {
+        return true;
+      }
+      return false;
+    }
+
     renderComponents();
-  },[])
+  }, []);
 
   return (
-    <>
-      {
-        showContent?.apiKey
-        ?
-        <BoxComponent apiKey={showContent?.apiKey} productIdentifier={showContent?.productIdentifier} hey={true} />
-        :
-        <div data-apikey="93ce02167b8a4cd3c6189decb476b06e2cd211765ba3521e" data-productidentifier="30005211" data-cf />
-      }
-    </>
-  )
+    <></>
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import BoxComponent from './components/BoxComponent';
+import App from './App';
 
-function renderComponents() {
-  const elements = document.querySelectorAll('div[data-cf][data-productIdentifier][data-apiKey]');
-  elements.forEach((element) => {
-    console.log(element);
-    const productIdentifier = element.getAttribute('data-productIdentifier');
-    const apiKey = element.getAttribute('data-apiKey');
-    if (validateApiKeys(apiKey, productIdentifier)) {
-      element.classList.add("gs__widget__wrapper")
-      const box = document.createElement('div');
-      const boxElement = <BoxComponent apiKey={apiKey} productIdentifier={productIdentifier} />;
-      ReactDOM.createRoot(element).render(boxElement);
-      element.appendChild(box);
-    }
-  });
-}
-
-  // Function to validate API keys (sample synchronous validation)
-  function validateApiKeys(apiKey, productIdentifier) {
-    // You might implement server-side validation here
-    // For example, making an API request to validate the keys
-
-    // Sample synchronous validation - Replace this with your actual validation logic
-    if (apiKey && productIdentifier) {
-      // Mock validation by checking if both API key and product identifier are present
-      return true;
-    }
-    return false;
-  }
-
-document.addEventListener('DOMContentLoaded', renderComponents);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
